cc1a0fe2addab8367119a8587c47dea6b9a609db
KazikKluz <kazik.kluz@gmail.com>
2020-08-10 20:25:30 +0100

    created and partially applied provider context pattern instead of redux 226

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,8 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
-import CartContext from "../../context/cart/cart.context";
-
+import { CartContext } from "../../providers/cart/cart.provider";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
