a5b6c93396d56e0670fefb7d7e8fc05971fd265f
KazikKluz <kazik.kluz@gmail.com>
2020-08-29 23:11:31 +0100

    more provider context pattern 2

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,14 +5,12 @@ import { withRouter } from "react-router-dom";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
-import { selectCartItems } from "../../redux/cart/cart.selectors";
-import { toggleCartHidden } from "../../redux/cart/cart.actions.js";
 import { CartContext } from "../../providers/cart/cart.provider";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ history, dispatch }) => {
-  const { cartItems } = useContext(CartContext);
+const CartDropdown = ({ history }) => {
+  const { cartItems, toggleHidden } = useContext(CartContext);
 
   return (
     <div className="cart-dropdown">
@@ -28,7 +26,7 @@ const CartDropdown = ({ history, dispatch }) => {
       <CustomButton
         onClick={() => {
           history.push("/checkout");
-          dispatch(toggleCartHidden());
+          toggleHidden();
         }}
       >
         GO TO CHECKOUT
@@ -37,8 +35,4 @@ const CartDropdown = ({ history, dispatch }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-});
-
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default withRouter(CartDropdown);
--- src/providers/cart/cart.provider.jsx
+++ src/providers/cart/cart.provider.jsx
@@ -1,6 +1,11 @@
 import React, { createContext, useState, useEffect } from "react";
 
-import { addItemToCart, removeItemFromCart } from "./cart.utils";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  filterItemsFromCart,
+  getCartItemsCount,
+} from "./cart.utils";
 
 export const CartContext = createContext({
   cartItems: [],
@@ -16,9 +21,26 @@ const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
   const [cartItemsCount, setCartItemsCount] = useState(0);
   const addItem = (item) => setCartItems(addItemToCart(cartItems, item));
+  const removeItem = (item) =>
+    setCartItems(removeItemFromCart(cartItems, item));
+  const clearItemFromCart = (item) =>
+    setCartItems(filterItemsFromCart(cartItems, item));
+
+  useEffect(() => {
+    setCartItemsCount(getCartItemsCount(cartItems));
+  }, [cartItems]);
+
   return (
     <CartContext.Provider
-      value={{ hidden, toggleHidden, cartItems, addItem, cartItemsCount }}
+      value={{
+        hidden,
+        toggleHidden,
+        cartItems,
+        addItem,
+        cartItemsCount,
+        removeItem,
+        clearItemFromCart,
+      }}
     >
       {children}
     </CartContext.Provider>
